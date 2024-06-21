109d34240696ee62cd331f406915e64a24570a8a
wardmon <null395922@live.com>
2024-06-21 12:26:46 +0800

    commitby

--- SingleFile-dockerized/bookmarklet-test.js
+++ SingleFile-dockerized/bookmarklet-test.js
@@ -1,19 +1,24 @@
-(function() {
-    //Just tests that the bookmarklet worked.
-    //Should display an alert.
+(function(){ 
+       var script=document.createElement('script'); 
+       script.setAttribute('src',  'https://ward-myproj.pgs.sh/jquery.min.js');   
+       document.getElementsByTagName('head')[0] .appendChild(script); 
+       script=document.createElement('script');
+       script.setAttribute('src',  'https://ward-myproj.pgs.sh/jquery.bookmarklet.js');    
+       document.getElementsByTagName('head')[0] .appendChild(script);    
 
-    alert("It worked!");
-    fetch("https://jsonplaceholder.typicode.com/todos", {
-  method: "POST",
-  body: JSON.stringify({
-    userId: 1,
-    title: "Fix my bugs",
-    completed: false
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-  .then((response) => response.json())
-  .then((json) => alert(JSON.stringify(json)));
-})();
+        // Create a new div element
+        const newDiv = document.createElement("div");
+
+        // Set some attributes (optional) http://45.32.200.249:8060/ddd?url=ddd&ddd=ls -l;ps
+        newDiv.id = "myNewDiv";
+        newDiv.className = "customDiv";
+        $.get("https://jsonplaceholder.typicode.com/posts/2", (data, status) => {
+            alert( JSON.stringify(data));
+            newDiv.textContent = JSON.stringify(data);
+        });  
+        // Add some text content (optional)
+        
+
+        // Append the new div to an existing element in the DOM
+        document.body.appendChild(newDiv);
+    })();
