605879731afc217c2ed3af6cf8cbed760daf730e
wardmon <null395922@live.com>
2025-02-11 19:23:24 +0800

    api upload demo

---
+++ 2025-02-11/Clip_20250211_185201.ts
@@ -0,0 +1,14 @@
+import { serve } from "./deps.ts";
+import handler from "./api/run.ts";
+serve(
+  async (request: Request): Promise<Response> => {
+    const response = await handler(request);
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Headers", "*");
+    response.headers.set("Access-Control-Allow-Methods", "*");
+    response.headers.set("Access-Control-Allow-Credentials", "true");
+    return response;
+  },
+  { port: 10030 },
+);
+console.log("server at", "http://localhost:10030");
